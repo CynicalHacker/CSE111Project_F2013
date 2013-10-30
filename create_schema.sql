ebfc19d8b696c9f8b4b2e1a928a103d53db9a424
Kevin <chipbuster@gmail.com>
2013-10-29 21:44:36 -0700

    Tested startdb.py, all SQL scripts are working, snazzy new licensing and info in the READMEH

--- create_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE lab (reactions varchar not null, labn decimal(2,0) not null);
-CREATE TABLE reaction (r_name varchar not null, casn decimal(10,0) not null);
-CREATE TABLE compound (casn decimal(10,0) not null, cname varchar not null, hazard char(4) not null);
-CREATE TABLE inventory (amount decimal(7,0) not null, units char (1) not null, casn decimal(10,0) not null);
-CREATE TABLE catalog (casn decimal(10,0) not null, csname varchar not null, price decimal(8,2) not null, amount decimal(10,2) not null, units char(1) not null);
-CREATE TABLE supplier (sname varchar not null, phonen decimal(10,0) not null, warehouse varchar not null);
-
