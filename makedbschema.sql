0654d7fe9b55d5b7c13ddeb220cedbc3fb89ed65
Kevin <chipbuster@gmail.com>
2013-10-31 16:15:40 -0700

    Modified startdb to include a login prompt. stardb will now authenticate users and pass them off to the appropriate user or admin shell.

--- makedbschema.sql
+++ makedbschema.sql
@@ -2,10 +2,10 @@
 --All statements must be on one line--
 --CREATE TABLE statements only. 
 
-CREATE TABLE lab (reactions varchar not null, labn decimal(2,0) not null);
-CREATE TABLE reaction (r_name varchar not null, casn decimal(10,0) not null);
-CREATE TABLE compound (casn decimal(10,0) not null, cname varchar not null, hazard char(4) not null);
-CREATE TABLE inventory (amount decimal(7,0) not null, units char (1) not null,  varchar name not null, casn decimal(10,0) not null,);
-CREATE TABLE catalog (casn decimal(10,0) not null, csname varchar not null, price decimal(8,2) not null, amount decimal(10,2) not null, units char(1) not null);
+CREATE TABLE lab (reaction varchar not null, labn decimal(2,0) not null);
+CREATE TABLE reaction (rname varchar not null, casn decimal(10,0) not null);
+CREATE TABLE compound (cname varchar not null, casn decimal(10,0) not null, hazard char(4) not null);
+CREATE TABLE inventory (amount decimal(7,0) not null, unit char (1) not null,  varchar cname not null, casn decimal(10,0) not null);
+CREATE TABLE catalog (casn decimal(10,0) not null, csname varchar not null, price decimal(8,2) not null, amount decimal(10,2) not null, unit char(1) not null);
 CREATE TABLE supplier (sname varchar not null, phonen decimal(11,0) not null, location varchar not null);
 
--- startdb.py
+++ startdb.py
@@ -3,48 +3,109 @@ import sys
 import os
 import sqlite3
 import traceback
+import hashlib
+import myadmin
+import myuser
 
-def conn_to_db(dbname): #Returns sqlite3 connection to dbname, none if error
+##Module to initialize db connection and authenticate the user.
+
+def login_user():
+	print("Enter databse name (blank for default):")
+	dbname = raw_input()
+	if dbname == "": #if blank, use default name
+		dbname = "chemdb" 
+	conn = conn_to_db(dbname) #attempt connection
+	if conn is None: #DB is missing
+		print("Database connection failed. Force creation[y/n]?")
+		cont = raw_input
+		if cont == "y":
+			conn = conn_to_db(dbname, True)
+			if conn is None: #Creation failed, probably missing config
+				print("Could not create database. Check error messages. Exiting...")
+				sys.exit(-5)
+			else:
+				print("Database creation succesful!")
+		else: #do not continue
+			print("Exiting...")
+			sys.exit(-5)
+	#We now have a connection to a database.
+	print("Database connection successful!")	
+	while(True):
+		print("Are you user or admin?")
+		user = raw_input()
+		if user == "user":
+			print("Welcome, user!")
+			myuser.main(conn) #Pass control to myuser.py
+			sys.exit(0) #If we return from myuser, exit the program
+		elif user == "admin":
+			print("Please enter the admin password:")
+			pwd = raw_input()
+			if isAdmin(pwd):
+				print("Welcome, Admin!")
+				myadmin.main(conn) #Pass control to myadmin.py
+				sys.exit(0) #If we return from myadmin, exit program
+			else:
+				print("Authentication failure!")
+		else:
+			print("Unrecognized user type!")
+
+def make_default_db():
+	return conn_to_db("chemdb", True)
+
+def conn_to_db(dbname, create=False): #Returns sqlite3 connection to dbname, None if error
 	try:
 		tmp  = open(dbname)		#If we find the database, close the file
-		tmp.close()
-		conn = sqlite3.connect(dbname) 	#in finally and return a sqlite3 connection
+		tmp.close()				#and return a sqlite3 connection
+		conn = sqlite3.connect(dbname) 
 		return conn
-	except IOError:			#If not, let's initialze the database with the
-		try:				#schema in create_schema.sql
-			makeschema = open("makedbschema.sql")
-			conn = sqlite3.connect(dbname)
-			c = conn.cursor()
-			print "Warning: you are about to create a new database with name %s."
-			print "Are you sure you want to do this (y/n)?"
-			inps = raw_input() #Do you really want to make another db?
-			if inps is 'y' or inps is 'yes':
+	except IOError:			#If not, check to see if we meant to create a db.
+		if create is True:	#Continue to make and pop db if we set create flag.
+			try: #Try to make db schema
+			
+				makeschema = open("makedbschema.sql")
+				conn = sqlite3.connect(dbname)
+				c = conn.cursor()
+				
 				for l in makeschema:
-					c.execute(l) #execute the contents per line. Requires that
-								#each CREATE statement is on one line.
-				conn.commit()
-				#We now have a connection and a cursor to a 
-				#freshly schema'd database Fill that sucker up!
+					c.execute(l) #Pipe statements one line at a time from file
+				conn.commit() 	 #and execute them. Must be one SQL per line.
+				
+			except sqlite3.OperationalError: #bad SQL code!
+				print "Warning! SQL Error detected! Please check makebdschema.sql for syntax errors! Stack trace below:"
+				traceback.print_exc()
+				return None #For safety
+			except IOError:
+				print("Warning! Population script is missing! Please make sure that makedbschema.sql is present!")
+				return None
+	
+			#At this point in the code, we have a cursor on a db with a schema.
+
+			try: #to populate newly-made DB
 				filldb = open("filldb.sql")
+				
 				for l in filldb:
 					c.execute(l)
-		
 				conn.commit()
-				return conn #Return connection to freshly populated database
-			else:
-				print "Aborting database creation."
-				sys.exit(-5)
 				
-	except IOError:
-		print("Can't create the database schema because files were not found.")
-		return
-	except sqlite3.OperationalError:
-		print("Incorrect SQL statement. Please check files for syntax errors.")
-		traceback.print_exc()
-		return
-
-
-			
-			
-
-
+				return conn #Return connection to freshly populated database
+			except IOError:
+				print "Warning! Population script is missing! Please make sure that filldb.sql is present!"
+				return None
+			except sqlite3.OperationalError:
+				print "Warning! SQL Error detected! Please check filldb.sql for syntax errors! Stack trace below:"
+				traceback.print_exc()
+				return None #For safety
+				
+		else: #We did not want to make a new database. Return None?
+			return None
+				
+def isAdmin(password):
+	#Password is "password". Returns true if it matches
+	adminhash = '\xf9\xe0H\xbeL\xd0$\xfc\x14<eR}#\xf2\x88\xfb?\xbb\xcfg\xda\x87tU\x9cl\x07\xcd^\xdd\x1dWtQ\xd1\xb1O\x86K\xf1\xca)\\\xd5\xce\xd8\x92\x95\x84\xca\x14\x98\xbex\xd6\x07\xcc5P\xe7L\xce\xca'
+	salt = 'kLi7@alIqDV!2b%."DcNLR$y!yfWkLY11b4l7trkA9j@!r5BDXcm_qDdiQMQd+vq_Ly#M.,ja@,B5g+eUk#FYlW8r8w0ff/Xz%w'
+	checker = hashlib.sha512()
+	checker.update(password + salt)
+	if checker.digest() == adminhash:
+		return True	
+	else:
+		return False
